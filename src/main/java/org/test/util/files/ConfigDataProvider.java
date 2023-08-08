1bd483e9cdca0afeab9a20a10b09300b8dd52a7d
Malith Senadheera <malith.akalanka@outlook.com>
2023-08-08 15:30:27 +0530

    test:updated project

--- src/main/java/org/test/util/files/ConfigDataProvider.java
+++ src/main/java/org/test/util/files/ConfigDataProvider.java
@@ -1,5 +1,35 @@
 package org.test.util.files;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigDataProvider {
 
+    private Properties pro;
+
+    public ConfigDataProvider(String configFilePath) {
+        loadConfig(configFilePath);
+    }
+
+    private void loadConfig(String configFilePath) {
+        pro = new Properties();
+        try (FileInputStream fis = new FileInputStream(configFilePath)) {
+            pro.load(fis);
+        } catch (IOException e) {
+            System.out.println("Not able to load config file: " + e.getMessage());
+        }
+    }
+
+    public String getDataFromConfig(String keyToSearch) {
+        return pro.getProperty(keyToSearch);
+    }
+
+    public String getBrowser() {
+        return getDataFromConfig("Browser");
+    }
+
+    public String getQAURL() {
+        return getDataFromConfig("QAURL");
+    }
 }
